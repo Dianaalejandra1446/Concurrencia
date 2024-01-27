73b52a8c7327b8ccee157c4e4893377dece71b9a
Diana Alejandra Diaz Niño <139186201+Dianaalejandra1446@users.noreply.github.com>
2024-01-27 12:21:57 -0500

    avance runnable

--- src/HiloRunnable.java
+++ src/HiloRunnable.java
@@ -1,3 +1,13 @@
-public class HiloRunnable {
-    
+public class HiloRunnable implements Runnable {
+      public void run(){
+        for(int i = 0;i<= 5;i++){
+            //Imprime el identificador de cada hilo con el currentThread
+            System.out.println("Hilo "+Thread.currentThread().getId()+ ": "+i);
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+                System.out.println("El hilo se ja interrumpido");
+            }
+        }
+    }
 }
