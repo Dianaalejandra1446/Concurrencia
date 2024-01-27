e4031824b6277290b196686634713a01ea2a89a2
Dianaalejandra1446 <dianaleja04@hotmail.com>
2024-01-27 12:39:01 -0500

    avance

--- src/Main2.java
+++ src/Main2.java
@@ -1,12 +1,13 @@
-public class Main2(){
-    public static void main(String[] args) throws Exception{
+public class Main2 {
+
+    public static void main(String[] args) {
         HiloRunnable runnable1 = new HiloRunnable();
         HiloRunnable runnable2 = new HiloRunnable();
-
+    
         Thread hilo1 = new Thread(runnable1);
         Thread hilo2 = new Thread(runnable2);
-
+    
         hilo1.start();
-        hilo1.start();
+        hilo2.start(); 
     }
 }
